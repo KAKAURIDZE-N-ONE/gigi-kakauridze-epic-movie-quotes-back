34ea75fba5e90ad3821d571b8c8813bb5ec4771e
KAKAURIDZE-N-ONE <g_kakauridze3@cu.edu.ge>
2025-02-21 17:27:00 +0400

    fix: remove another comments

--- app/Http/Resources/QuoteResource.php
+++ app/Http/Resources/QuoteResource.php
@@ -17,7 +17,7 @@ class QuoteResource extends JsonResource
 		return [
 			'id'             => $this->id,
 			'quote'          => $this->quote,
-			'image'          => $this->getFirstMediaUrl('images'), // ✅ Fix: Load image from Spatie Media Library
+			'image'          => $this->getFirstMediaUrl('images'),
 			'likes_count'    => $this->likes_count,
 			'comments_count' => $this->comments_count,
 		];
