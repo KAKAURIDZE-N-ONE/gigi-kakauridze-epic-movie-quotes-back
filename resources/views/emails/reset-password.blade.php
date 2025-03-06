b235aad14736272e73febcbdb7b07b6394e570d7
KAKAURIDZE-N-ONE <g_kakauridze3@cu.edu.ge>
2025-03-06 12:17:37 +0400

    style: update email style

--- resources/views/emails/reset-password.blade.php
+++ resources/views/emails/reset-password.blade.php
@@ -5,7 +5,9 @@
 </head>
 <body style="background-color: #181623;">
     <div style="color: white; padding: 0 40px; margin-top: 20px;">
-        <img style="display: inline-block; margin: 0 auto; margin-top: 20px;" src="{{ env('APP_URL') . '/storage/assets/Logo.png' }}" alt="Logo">
+    <img style="display: inline-block; margin: auto; margin-top: 20px;" 
+     src="{{ url('storage/assets/icon.png') }}" 
+     alt="Logo">
         <p style="margin-top: 32px;">Hola {{$name}}!</p>
         <p style="margin-top: 22px;">Thanks for joining Movie quotes! We really appreciate it. Please click the button below to verify your account:</p>
         <a href="{{ $url }}" style="margin-top: 22px; display:inline-block; padding:10px 20px; background-color:#E31221; color:#fff; text-decoration:none; border-radius:5px;">
@@ -17,5 +19,6 @@
         <p style="margin-top: 32px;">If you have any problems, please contact us: support@moviequotes.ge</p>
         <p style="margin-top: 22px;">MovieQuotes Crew</p>
     </div>
+    <div style="width: 100%; height:10px"></div>
 </body>
 </html>
